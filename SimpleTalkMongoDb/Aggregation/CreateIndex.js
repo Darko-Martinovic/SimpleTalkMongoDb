8e451fa4938b9e125b794ae73392b942b4821894
Darko-Martinovic <darko.martinovic@outlook.com>
2018-10-03 14:17:13 +0200

    Small changes

--- SimpleTalkMongoDb/Aggregation/CreateIndex.js
+++ SimpleTalkMongoDb/Aggregation/CreateIndex.js
@@ -1,4 +1,4 @@
 ﻿db.adventureWorks2016.createIndex(
-    { CustomerId: 1, TerritoryId: 1, TotalDue: 1, _id: 1 })
+    { CustomerId: 1, TerritoryId: 1, TotalDue: 1})
 db.adventureWorks2016.createIndex(
     { TotalDue: -1 })
